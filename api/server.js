fe3b75789dee54c6ab6f1586beb20674b7ec103c
Julian Leon <jleon1992@gmail.com>
2020-08-11 18:02:52 -0400

    endpoints created for sales

--- api/server.js
+++ api/server.js
@@ -84,6 +84,61 @@ server.get('/sales', (req, res) => {
         .then(sales => {
             res.status(200).json({ sales: sales})
         })
+        .catch(err => {
+            console.log(err)
+        })
 })
 
+server.post('/sales', (req, res) => {
+    const sale = req.body
+    db('sales')
+    .insert(sale)
+    .then(ids => {
+        res.status(201).json({inserted: ids})
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
+
+server.put('/sales/:id', (req, res) => {
+    const id = req.params.id
+    const changes = req.body
+
+    db('sales')
+        .where({ id: id })
+        .update(changes)
+        .then(count => {
+            if (count) {
+                res.status(200).json({ message: "updated successfully" });
+            } else {
+                res.status(404).json({ message: "not found" });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+
+            res.status(500).json({ error: error.message });
+        });
+})
+
+server.delete('/sales/:id', (req, res) => {
+    const id = req.params.id
+    db('sales')
+        .where({ id: id })
+        .delete()
+        .then(count => {
+            if (count) {
+                res.status(200).json({ message: "removed successfully" });
+            } else {
+                res.status(404).json({ message: "not found" });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+
+            res.status(500).json({ error: error.message });
+        });
+
+})
 module.exports = server;
